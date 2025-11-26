7eb2ff4b679272ba25cbdfc84712416a6c261f82
Matus Gavornik <mgavorni@c3r7s2.42prague.com>
2025-11-26 15:11:34 +0100

    no

--- cpp2/ex00/src/Fixed.cpp
+++ cpp2/ex00/src/Fixed.cpp
@@ -1,4 +1,4 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   Fixed.cpp                                          :+:      :+:    :+:   */
@@ -6,33 +6,42 @@
 /*   By: mgavorni <mgavorni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 14:32:17 by mgavorni          #+#    #+#             */
-/*   Updated: 2025/11/14 15:00:28 by mgavorni         ###   ########.fr       */
+/*   Updated: 2025/11/26 15:04:20 by mgavorni         ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 #include "Fixed.hpp"
+#include "Colors.hpp"
 
-Fixed::Fixed(void)
+Fixed::Fixed(void) : ctor_(Default)
 {
-    std::cout << "Default constructor called" << std::endl;
+    std::cout << RED <<"Default constructor called"<< RESET << std::endl;
     this->_fixedPointValue = 0;
 }
 
-Fixed::Fixed(const Fixed &other)
+Fixed::Fixed(const Fixed &other) : ctor_(Copy)
 {
-    std::cout << "Copy constructor called" << std::endl;
+    std::cout << YELLOW << "Copy constructor called"<< RESET << std::endl;
     *this = other;
 }
 
 Fixed::~Fixed(void)
 {
-    std::cout << "Destructor called" << std::endl;
+    std::cout << MAGENTA << "Destructor called"<< RESET << std::endl;
+    switch (ctor_) {
+            case Default:   std::cout<<RED << "Default Constructor\n"<< RESET; break;
+            case Copy:   std::cout <<YELLOW<< "Copy Constructor\n" << RESET; break;
+            case CopyAssignment:std::cout<<GREEN << "CopyAssignment operator\n"<< RESET; break;
+            case MemberFunction1:std::cout << "MemberFunction1\n"; break;
+            case MemberFunction2:std::cout << "MemberFunction2\n"; break;
+        }
 }
 
 Fixed &Fixed::operator=(const Fixed &other)
 {
-    std::cout << "Copy assignment operator called" << std::endl;
+    std::cout << GREEN <<"Copy assignment operator called"<< RESET << std::endl;
     this->_fixedPointValue = other.getRawBits();
+    this->ctor_ = CopyAssignment;
     return (*this);
 }
 
@@ -46,4 +55,6 @@ void Fixed::setRawBits(int const raw)
 {
     std::cout << "setRawBits member function called" << std::endl;
     this->_fixedPointValue = raw;
+    this->ctor_ = MemberFunction2;
 }
+
